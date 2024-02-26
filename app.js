073a5fd34e6afdbb1e55e2a35d380a34161b4aa1
tonyishangu <tonyishangu@gmail.com>
2024-02-26 23:00:04 +0300

    make a get request using a api

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
-const express = require('express')
- 
-const app = express()
+const express = require('express');
+const fs = require('fs')
+
+const app = express();
+
+// app.get('/', (req, res) => {
+//   res.status(200).json({ message: 'hello from the server', app: 'natours' });
+// });
+
+// app.post('/', (req, res) => {
+//     res.send('you can post using this endpoints')
+// })
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
+
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: {
+            tours
+        }
+    })
+})
+
+const port = 5000;
+app.listen(port, () => {
+  console.log(`app is runing on port ${port}...`);
+});
