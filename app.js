5fc8598e3d9d83720fd11f56fba03cc43e6242d4
tonyishangu <tonyishangu@gmail.com>
2024-02-27 23:15:07 +0300

    handle post request

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fs = require('fs')
 
 const app = express();
+app.use(express.json());
 
 // app.get('/', (req, res) => {
 //   res.status(200).json({ message: 'hello from the server', app: 'natours' });
@@ -20,6 +21,21 @@ app.get('/api/v1/tours', (req, res) => {
         data: {
             tours
         }
+    })
+})
+
+app.post('/api/v1/tours', (req, res) => {
+    // console.log(req.body)
+    const newId = tours[tours.length - 1].id + 1
+    const newTour = Object.assign(req.body, {id: newId})
+    tours.push(newTour)
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err =>{
+        res.status(201).json({
+            status:'created',
+            data:{
+                tour: newTour
+            }
+        })
     })
 })
 
